f3a7670a08cdfbfee39071d2391759dc67943b85
Alex Auritt <alexauritt@yahoo.com>
2014-10-26 15:54:12 -0400

    Hand uses BestHandFinder

--- lib/hand.rb
+++ lib/hand.rb
@@ -17,8 +17,7 @@ class Hand
 	end
 
 	def best_hand
-		HandEvaluator.evaluate(@cards)
-		# @best_hand ||= BestPlayingHandFinder.perform(cards)
+		@best_hand ||= BestPlayingHandFinder.perform(@cards)
 	end
 
 	def errors
@@ -27,7 +26,6 @@ class Hand
 
 	private
 	class BestPlayingHandFinder
-		
 		def self.perform(cards)
 			best_hand = FoldedHand.new			
 			unless folded? cards
@@ -36,86 +34,11 @@ class Hand
 					best_hand = new_hand if new_hand > best_hand
 				end
 			end
+			best_hand
 		end
 
 		def self.folded?(cards)
 			cards.length < 7
-		end
-
-	end
-	class HandEvaluator
-		def self.evaluate(cards)
-			if has_straight_flush?(cards)
-				return :straight_flush
-			elsif has_four_of_a_kind?(cards)
-				return :four_kind
-			elsif has_full_house?(cards)
-				return :full_house
-			elsif has_flush?(cards)
-				return :flush
-			elsif has_straight?(cards)
-				return :straight
-			elsif has_three_of_a_kind?(cards)
-				return :three_kind
-			elsif has_two_pair?(cards)
-				return :two_pair
-			elsif has_single_pair?(cards)
-				return :pair
-			end
-			return :high_card
-		end
-
-		def self.has_straight_flush?(cards)
-			has_straight?(cards) && has_flush?(cards)
-		end
-
-		def self.has_four_of_a_kind?(cards)
-			group_by_value(cards).select {|card_value, count| count == 4}.length > 0
-		end
-
-		def self.has_full_house?(cards)
-			grouped = group_by_value(cards)
-			more_than_one_pair = grouped.select {|card_value, count| count >= 2}.length > 1
-			has_three_of_a_kind?(cards) && more_than_one_pair		
-		end
-
-		def self.has_flush?(cards)
-			suit_count = cards.map {|c| c[1] }.each_with_object(Hash.new(0)) { |suit, counts| counts[suit] += 1 }
-			suit_count.any? { |card_value, count| count >= 5 }
-		end
-
-		def self.has_straight?(cards)
-			sorted_indexes = cards.map {|c| Card::VALUES.find_index c[0] }.uniq.sort
-			return true if sorted_indexes.size > 4 && (sorted_indexes[4] - sorted_indexes[0] == 4)
-			return true if sorted_indexes.size > 5 && (sorted_indexes[5] - sorted_indexes[1] == 4)
-			return true if sorted_indexes.size > 6 && (sorted_indexes[6] - sorted_indexes[2] == 4)
-			return true if broadway_straight?(cards)
-			false
-		end
-
-		def self.broadway_straight?(cards)
-			sorted_indexes = cards.map {|c| Card::VALUES.find_index c[0] }.uniq.sort
-			count = sorted_indexes.count
-			top = sorted_indexes.last
-			count >= 5 && sorted_indexes.first == 0 && top == 12 && sorted_indexes[count - 4] == 9
-		end
-
-		def self.has_three_of_a_kind?(cards)
-			group_by_value(cards).select {|card_value, count| count == 3}.length > 0
-		end
-
-		def self.has_two_pair?(cards)
-			group_by_value(cards).select {|card_value, count| count == 2}.length > 1
-		end
-
-		def self.has_single_pair?(cards)
-			card_values = cards.map {|c| c[0] }
-			card_values.length - 1 == card_values.uniq.length
-		end
-
-		def self.group_by_value(cards)
-			card_values = cards.map {|c| c[0] }
-			card_values.each_with_object(Hash.new(0)) { |card_val,counts| counts[card_val] += 1 }
 		end
 	end
 
--- spec/hand_spec.rb
+++ spec/hand_spec.rb
@@ -28,53 +28,52 @@ describe "Hand" do
 
 	describe "best_hand" do
 		it 'can identify high card' do
-			expect(Hand.new("Ac 2d 3h 5s 6c 7d Td").best_hand).to eq(:high_card)
+			expect(Hand.new("Ac 2d 3h 5s 6c 7d Td").best_hand).to be_instance_of HighCard
 		end
 
 		it 'can identify pair' do
-			expect(Hand.new("Ac Ad 3h 5s 6c 7d Td").best_hand).to eq(:pair)
+			expect(Hand.new("Ac Ad 3h 5s 6c 7d Td").best_hand).to be_instance_of Pair
 		end
 
 		it 'can identify two pair' do
-			expect(Hand.new("Ac Ad 3h 3s 6c 7d Td").best_hand).to eq(:two_pair)
-			expect(Hand.new("Ac Ad 3h 3s 6c 6d Td").best_hand).to eq(:two_pair)
+			expect(Hand.new("Ac Ad 3h 3s 6c 7d Td").best_hand).to be_instance_of TwoPair
+			expect(Hand.new("Ac Ad 3h 3s 6c 6d Td").best_hand).to be_instance_of TwoPair
 		end
 
 		it 'can identiy three of a kind' do
-			expect(Hand.new("Ac Ad Ah 3s 6c 7d Td").best_hand).to eq(:three_kind)
+			expect(Hand.new("Ac Ad Ah 3s 6c 7d Td").best_hand).to be_instance_of ThreeOfAKind
 		end
 
 		it 'can find a low straight' do
-			expect(Hand.new("Ac 2d 3h 4s 5c 7d Td").best_hand).to eq(:straight)
+			expect(Hand.new("Ac 2d 3h 4s 5c 7d Td").best_hand).to be_instance_of Straight
 		end
 
 		it 'can find a mid straight' do
-			expect(Hand.new("Ac 7d 6h Js 5s 8d 9d").best_hand).to eq(:straight)
+			expect(Hand.new("Ac 7d 6h Js 5s 8d 9d").best_hand).to be_instance_of Straight
 		end
 
 		it 'can find a high straight' do
-			expect(Hand.new("Ac 2d Qh Js 5s Kd Td").best_hand).to eq(:straight)
+			expect(Hand.new("Ac 2d Qh Js 5s Kd Td").best_hand).to be_instance_of Straight
 		end
 
 		it 'can find a flush' do
-			expect(Hand.new("Ac 2c 3c 4s 6c 7c Td").best_hand).to eq(:flush)
+			expect(Hand.new("Ac 2c 3c 4s 6c 7c Td").best_hand).to be_instance_of Flush
 		end
 
 		it 'can find full house' do
-			expect(Hand.new("Ac Ad Ah 3s 3c 3d Td").best_hand).to eq(:full_house)
+			expect(Hand.new("Ac Ad Ah 3s 3c 3d Td").best_hand).to be_instance_of FullHouse
 		end
 
 		it 'can find four of a kind' do
-			expect(Hand.new("Ac Ad Ah As 3c 3d Td").best_hand).to eq(:four_kind)
+			expect(Hand.new("Ac Ad Ah As 3c 3d Td").best_hand).to be_instance_of FourOfAKind
 		end
 
 		it 'can find straight flush' do
-			expect(Hand.new("Ac 2c 3c 4c 5c 3d Td").best_hand).to eq(:straight_flush)
+			expect(Hand.new("Ac 2c 3c 4c 5c 3d Td").best_hand).to be_instance_of StraightFlush
 		end
 
 		it 'does not falsely identify straight flush if straight and flush are not same 5 cards' do
-			pending 'bug must fix'
-			expect(Hand.new("Ac 2c 3c 4c 5d Tc Td").best_hand).to eq(:flush)
+			expect(Hand.new("Ac 2c 3c 4c 5d Tc Td").best_hand).to be_instance_of Flush
 		end
 	end
 end
