5cb1a689878638ff2ebad0b887755fc75f915f55
Alex Auritt <alexauritt@yahoo.com>
2014-10-26 15:48:21 -0400

    playing hand builder

--- lib/playing_hand_builder.rb
+++ lib/playing_hand_builder.rb
@@ -1,20 +1,42 @@
 require 'straight_flush'
+require 'four_of_a_kind'
+require 'full_house'
+require 'flush'
+require 'straight'
+require 'three_of_a_kind'
+require 'two_pair'
+require 'pair'
+require 'high_card'
 require 'card'
 
 class PlayingHandBuilder
 	def self.build(cards)
+		return FoldedHand.new unless cards.size == 5
 		return StraightFlush.new(cards) if straight_flush?(cards)
 		return FourOfAKind.new(cards) if four_kind?(cards)
+		return FullHouse.new(cards) if full_house?(cards)
+		return Flush.new(cards) if flush?(cards)
+		return Straight.new(cards) if straight?(cards)
+		return ThreeOfAKind.new(cards) if three_of_a_kind?(cards)
+		return TwoPair.new(cards) if two_pair?(cards)
+		return Pair.new(cards) if single_pair?(cards)
+		HighCard.new(cards)
 	end
 
 	private
-	
+
+		def self.straight_flush?(cards)
+			flush?(cards) && straight?(cards)
+		end
+
 		def self.four_kind?(cards)
 			group_by_value(cards).select {|card_value, count| count == 4}.length > 0
 		end
 
-		def self.straight_flush?(cards)
-			flush?(cards) && straight?(cards)
+		def self.full_house?(cards)
+			grouped = group_by_value(cards)
+			more_than_one_pair = grouped.select {|card_value, count| count >= 2}.length > 1
+			three_of_a_kind?(cards) && more_than_one_pair		
 		end
 
 		def self.flush?(cards)
@@ -35,10 +57,21 @@ class PlayingHandBuilder
 			count >= 5 && sorted_indexes.first == 0 && top == 12 && sorted_indexes[count - 4] == 9
 		end
 
+		def self.three_of_a_kind?(cards)
+			group_by_value(cards).select {|card_value, count| count == 3}.length > 0
+		end
+
+		def self.two_pair?(cards)
+			group_by_value(cards).select {|card_value, count| count == 2}.length > 1
+		end
+
+		def self.single_pair?(cards)
+			card_values = cards.map {|c| c[0] }
+			card_values.length - 1 == card_values.uniq.length
+		end
 
 		def self.group_by_value(cards)
 			card_values = cards.map {|c| c[0] }
 			card_values.each_with_object(Hash.new(0)) { |card_val,counts| counts[card_val] += 1 }
 		end
-
 end
--- spec/playing_hand_builder_spec.rb
+++ spec/playing_hand_builder_spec.rb
@@ -11,4 +11,40 @@ describe 'PlayingHandBuilder' do
 		expect(PlayingHandBuilder.build("As Ac Ad Ah 5s".split)).to be_instance_of FourOfAKind
 		expect(PlayingHandBuilder.build("Ac 3d 3h 3s 3c".split)).to be_instance_of FourOfAKind
 	end
+
+	it 'can build high card' do
+		expect(PlayingHandBuilder.build("Ac 2d 3h 5s 6c".split)).to be_instance_of HighCard
+	end
+
+	it 'can build pair' do
+		expect(PlayingHandBuilder.build("Ac Ad 3h 5s 6c".split)).to be_instance_of Pair
+	end
+
+	it 'can build two pair' do
+		expect(PlayingHandBuilder.build("Ac Ad 3h 3s 6c".split)).to be_instance_of TwoPair
+	end
+
+	it 'can build three of a kind' do
+		expect(PlayingHandBuilder.build("Ac Ad Ah 3s 6c".split)).to be_instance_of ThreeOfAKind
+	end
+
+	it 'can build a low straight' do
+		expect(PlayingHandBuilder.build("Ac 2d 3h 4s 5c".split)).to be_instance_of Straight
+	end
+
+	it 'can build a mid straight' do
+		expect(PlayingHandBuilder.build("4c 7d 6h 8s 5s".split)).to be_instance_of Straight
+	end
+
+	it 'can build a high straight' do
+		expect(PlayingHandBuilder.build("Ac Kd Qh Ts Js".split)).to be_instance_of Straight
+	end
+
+	it 'can build a flush' do
+		expect(PlayingHandBuilder.build("Ac 2c 3c 6c 7c".split)).to be_instance_of Flush
+	end
+
+	it 'can build full house' do
+		expect(PlayingHandBuilder.build("Ac Ad Ah 3s 3c".split)).to be_instance_of FullHouse
+	end
 end
