876ae257c2b032ff02040c0d6ad031a05a3c6296
Alex Auritt <alexauritt@yahoo.com>
2014-10-26 13:25:09 -0400

    some checks on method missing

--- lib/playing_hand.rb
+++ lib/playing_hand.rb
@@ -1,27 +1,16 @@
 class PlayingHand
 	attr_reader :card
-	# comparator_list = ['<','>','<=','>=','!=']
 	def method_missing(meth, *args)
-		# puts "args #{meth.class} #{self.hand_ranking} #{args.first.hand_ranking}"
-		# puts "does it #{comparator_list.include?(meth.to_s)}"
-		# if !comparator_list.include?(meth.to_s)
-		# 	puts "no found"
-		# 	super
-		# end
-		# self.hand_ranking.meth args.first.hand_ranking
-		# super
-		# false
-		self.hand_ranking.send(meth, args.first.hand_ranking)
-		# self.hand_ranking.send(meth, args.first.hand_ranking)
+		if Numeric.respond_to?(meth) && args.length == 1 && args.first.kind_of?(PlayingHand)
+			self.hand_ranking.send(meth, args.first.hand_ranking)
+		else
+			super
+		end
 	end
 
 	def ==(other_playing_hand)
 		self.hand_ranking == other_playing_hand.hand_ranking
 	end
-
-	# def !=(other_playing_hand)
-	# 	self.hand_ranking != other_playing_hand.hand_ranking
-	# end
 
 	def hand_ranking
 		raise "attempt to call hand_ranking on abstract class"
