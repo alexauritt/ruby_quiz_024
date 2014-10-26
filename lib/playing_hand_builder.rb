65df2b12a9f923cd6f230b192519c6409bb0b788
Alex Auritt <alexauritt@yahoo.com>
2014-10-26 15:31:28 -0400

    more tests

--- lib/playing_hand_builder.rb
+++ lib/playing_hand_builder.rb
@@ -28,6 +28,14 @@ class PlayingHandBuilder
 			false
 		end
 
+		def self.broadway_straight?(cards)
+			sorted_indexes = cards.map {|c| Card::VALUES.find_index c[0] }.uniq.sort
+			count = sorted_indexes.count
+			top = sorted_indexes.last
+			count >= 5 && sorted_indexes.first == 0 && top == 12 && sorted_indexes[count - 4] == 9
+		end
+
+
 		def self.group_by_value(cards)
 			card_values = cards.map {|c| c[0] }
 			card_values.each_with_object(Hash.new(0)) { |card_val,counts| counts[card_val] += 1 }
--- spec/playing_hand_builder_spec.rb
+++ spec/playing_hand_builder_spec.rb
@@ -2,12 +2,13 @@ require 'playing_hand_builder'
 
 describe 'PlayingHandBuilder' do
 	it 'should build a straight flush' do
-		straight_flush = "As 2s 3s 4s 5s".split
-		expect(PlayingHandBuilder.build(straight_flush).class).to eq(StraightFlush)
+		expect(PlayingHandBuilder.build("As 2s 3s 4s 5s".split)).to be_instance_of StraightFlush
+		expect(PlayingHandBuilder.build("Jc Tc Ac Kc Qc".split)).to be_instance_of StraightFlush
 	end
 
 	it 'should build four of a kind' do
-		four_kind = "As Ac Ad Ah 5s".split
-		expect(PlayingHandBuilder.build(four_kind).class).to eq(FourOfAKind)
+		four_kind = 
+		expect(PlayingHandBuilder.build("As Ac Ad Ah 5s".split)).to be_instance_of FourOfAKind
+		expect(PlayingHandBuilder.build("Ac 3d 3h 3s 3c".split)).to be_instance_of FourOfAKind
 	end
 end
